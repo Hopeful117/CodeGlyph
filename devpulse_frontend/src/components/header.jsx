d0a688e237b0e1acd2f3d599e9eee835e7ab3dab
Ludovic Brot <ludo@pop-os.home>
2025-06-19 03:58:12 +0200

    quick fix

--- devpulse_frontend/src/components/header.jsx
+++ devpulse_frontend/src/components/header.jsx
@@ -14,7 +14,7 @@ export default function Header(){
   };
     return(
         <header className="header">
-            <h1 className="ibm-plex-sans-title">Devpulse</h1>
+            <h1 className="ibm-plex-sans-title">CodeGlyph</h1>
             <nav className="ibm-plex-sans-title">
             <NavLink to={'/'}>Home</NavLink>
             <NavLink to={'/about'}>About</NavLink>
