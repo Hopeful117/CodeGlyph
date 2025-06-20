4c68ec53efef2dcecd43a2c43f8d3d154bd6a855
Ludovic Brot <ludo@pop-os.home>
2025-06-20 02:07:25 +0200

    fix fonction bookmark

--- devpulse_frontend/src/components/SaveButton.jsx
+++ devpulse_frontend/src/components/SaveButton.jsx
@@ -3,7 +3,7 @@
 import { useSavedArticle } from "../context/bookmarkContext";
 
 export default function SaveButton({url}){
-const {savedUrls,toogleSave}=useSavedArticle()
+const {savedUrls,toggleSave}=useSavedArticle()
 const isSaved = savedUrls.includes(url);
 
   
@@ -12,7 +12,7 @@ const isSaved = savedUrls.includes(url);
 
 
     return(
-        <button className='filter-button' onClick={()=>toogleSave(url)}>{isSaved ?'forget':'save'}</button>
+        <button className='filter-button' onClick={()=>toggleSave(url)}>{isSaved ?'forget':'save'}</button>
     )
 
 }
